4da7fb1009d5d41af6ae5334099bb5a8b82141f6
SangChu04 <drchasekim@gmail.com>
2020-03-30 23:26:07 +0900

    Add dropout

--- rnn_predictor.py
+++ rnn_predictor.py
@@ -16,17 +16,17 @@ class RNNPredictor(tf.keras.Model):
         super(RNNPredictor, self).__init__()
 
         self.units = units
-        self.gru1 = tf.keras.layers.GRU(units, return_sequences=True)
-        self.gru2 = tf.keras.layers.GRU(units, return_sequences=True)
-        self.gru3 = tf.keras.layers.GRU(units, return_sequences=True)
-        self.gru4 = tf.keras.layers.GRU(units)
+        self.gru1 = tf.keras.layers.GRU(units, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)
+        self.gru2 = tf.keras.layers.GRU(units, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)
+        self.gru3 = tf.keras.layers.GRU(units, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)
+        self.gru4 = tf.keras.layers.GRU(units, dropout=0.2, recurrent_dropout=0.2)
         self.dense = tf.keras.layers.Dense(1)
 
     def call(self, inputs, training=None, mask=None):
-        x = self.gru1(inputs)
-        x = self.gru2(x)
-        x = self.gru3(x)
-        x = self.gru4(x)
-        x = self.dense(x)
+        x = self.gru1(inputs, training=training)
+        x = self.gru2(x, training=training)
+        x = self.gru3(x, training=training)
+        x = self.gru4(x, training=training)
+        x = self.dense(x, training=training)
 
         return x
