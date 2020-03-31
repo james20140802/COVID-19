1df8915a98087e514bd9636520b4b1408e8b7c4a
SangChu04 <drchasekim@gmail.com>
2020-03-31 23:57:52 +0900

    Delete many gru layers because of over fitting

--- rnn_predictor.py
+++ rnn_predictor.py
@@ -17,16 +17,12 @@ class RNNPredictor(tf.keras.Model):
 
         self.units = units
         self.gru1 = tf.keras.layers.GRU(units, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)
-        self.gru2 = tf.keras.layers.GRU(units, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)
-        self.gru3 = tf.keras.layers.GRU(units, return_sequences=True, dropout=0.2, recurrent_dropout=0.2)
-        self.gru4 = tf.keras.layers.GRU(units, dropout=0.2, recurrent_dropout=0.2)
+        self.gru2 = tf.keras.layers.GRU(units, dropout=0.2, recurrent_dropout=0.2)
         self.dense = tf.keras.layers.Dense(1)
 
     def call(self, inputs, training=None, mask=None):
         x = self.gru1(inputs, training=training)
         x = self.gru2(x, training=training)
-        x = self.gru3(x, training=training)
-        x = self.gru4(x, training=training)
         x = self.dense(x, training=training)
 
         return x
