58d0420dc8ffbc07c9514959e3e79cb1b439c89f
SangChu04 <drchasekim@gmail.com>
2020-04-01 23:10:34 +0900

    Increase validaion data to 3

--- train.py
+++ train.py
@@ -5,22 +5,29 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import numpy as np
 
 from data_loader import load_train_data, univariate_data
 from rnn_predictor import RNNPredictor
 
 raw_data = load_train_data()
 data, labels = univariate_data(raw_data["ConfirmedCases"], 0, None, 5, 0)
-val_data, val_label = data[-1], labels[-1]
-train_data, train_labels = data[:-1], labels[:-1]
+
+val_data, val_labels = [list(data[20]), list(data[40]), list(data[63])], [labels[20], labels[40], labels[63]]
+train_data, train_labels = np.delete(data, [20, 40, 63], axis=0), np.delete(labels, [20, 40, 63], axis=0)
+
+val_data_set = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
 data_set = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+
+data_set = data_set.shuffle(len(list(data_set.as_numpy_iterator())), reshuffle_each_iteration=True)
+data_set = data_set.repeat(5)
 
 predictor = RNNPredictor(256)
 
 epochs = range(10000)
 
 loss_object = tf.keras.losses.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam(lr=1e-2)
+optimizer = tf.keras.optimizers.Adam(lr=1e-2, clipvalue=1)
 
 
 def loss(model, x, y, training=True):
@@ -39,13 +46,13 @@ def grad(model, inputs, targets):
 train_loss_results = []
 best_loss = float("inf")
 same_loss_count = 0
-val_loss = 0
 
 if tf.io.gfile.exists("./checkpoint/rnn_predictor.index"):
     predictor.load_weights("./checkpoint/rnn_predictor")
 
 for epoch in epochs:
     epoch_loss_avg = tf.keras.metrics.Mean()
+    epoch_val_loss_avg = tf.keras.metrics.Mean()
 
     for data, labels in data_set.batch(32):
         loss_value, grads = grad(predictor, tf.cast(data, tf.float32), tf.cast(labels, tf.float32))
@@ -55,17 +62,20 @@ for epoch in epochs:
 
     train_loss_results.append(epoch_loss_avg)
 
-    val_loss = loss(predictor, tf.expand_dims(val_data, 0), [val_label], training=False)
+    for data, label in val_data_set.batch(1):
+        val_loss = loss(predictor, [data], [label], training=False)
+        epoch_val_loss_avg(val_loss)
 
-    if val_loss < best_loss:
+    if epoch_val_loss_avg.result() < best_loss:
         predictor.save_weights("./checkpoint/rnn_predictor")
-        best_loss = val_loss
+        best_loss = epoch_val_loss_avg.result()
         same_loss_count = 0
     else:
         same_loss_count += 1
 
-    print("Epoch: {:d} Loss: {:f} Validation Loss: {:f}".format((epoch+1), epoch_loss_avg.result(), val_loss))
+    print("Epoch: {:d} Loss: {:f} Validation Loss: {:f}".format((epoch+1), epoch_loss_avg.result(),
+                                                                epoch_val_loss_avg.result()))
 
-    if same_loss_count > 1000:
+    if same_loss_count > 500:
         print("End the training. Best Loss: {:f}.".format(best_loss))
         break
